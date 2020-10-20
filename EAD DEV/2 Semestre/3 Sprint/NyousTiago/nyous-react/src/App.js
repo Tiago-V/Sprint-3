59410806bd807802a130810e2392d6dc60f0fd03
FIC <FIC@info.local>
2020-10-20 11:00:03 -0300

    up

--- EAD DEV/2 Semestre/3 Sprint/NyousTiago/nyous-react/src/App.js
+++ EAD DEV/2 Semestre/3 Sprint/NyousTiago/nyous-react/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Rodape from './components/rodape';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Nyous</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Rodape />
     </div>
   );
 }
---
+++ EAD DEV/2 Semestre/3 Sprint/NyousTiago/nyous-react/src/components/rodape.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Rodape(){
+    return(
+        <footer>
+            <h1>Nyous</h1>
+            <p>Senai Informática 132</p>
+        </footer>
+    )
+}
+
+export default Rodape;
