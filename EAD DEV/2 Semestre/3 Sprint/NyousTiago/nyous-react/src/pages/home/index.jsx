87f5d8397f2b100bc236bd0d34ad7e228cb3ceff
Tiago Valim <tiago.valim@gmail.com>
2020-10-22 17:27:20 -0300

    aula 05

---
+++ EAD DEV/2 Semestre/3 Sprint/NyousTiago/nyous-react/src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from '../../components/menu';
+import Rodape from '../../components/rodape';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Menu />
+            <Rodape />
+        </div>
+    )
+    
+}
+
+export default Home;
