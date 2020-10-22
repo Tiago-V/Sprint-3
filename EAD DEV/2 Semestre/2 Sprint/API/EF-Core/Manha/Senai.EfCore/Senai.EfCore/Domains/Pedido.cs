9e111b6b40eb24e778dec23593b54703cc025f03
Tiago Valim <tiago.valim@gmail.com>
2020-10-22 17:21:17 -0300

    aula 15

---
+++ EAD DEV/2 Semestre/2 Sprint/API/EF-Core/Manha/Senai.EfCore/Senai.EfCore/Domains/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Senai.EfCore.Domains
+{
+    public class Pedido : BaseDomain
+    {
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        //Propriedade referente ao relacionamento na classe PedidoItem com a classe Pedido
+        public List<PedidoItem> PedidosItens { get; set; }
+    }
+}
---
+++ EAD DEV/2 Semestre/2 Sprint/API/EF-Core/Manha/Senai.EfCore/Senai.EfCore/Domains/Produto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Senai.EfCore.Domains
+{
+    /// <summary>
+    /// Domínio referente ao produto
+    /// </summary>
+    public class Produto : BaseDomain
+    {
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        [NotMapped] //Não mapeia a propriedade no banco de dados
+        [JsonIgnore] //Ignora propriedade no retorno no Json
+        public IFormFile Imagem { get; set; }
+
+        //Url da imagem do produto salva no servidor
+        public string UrlImagem { get; set; }
+
+        //Propriedade referente ao relacionamento na classe PedidoItem com a classe Pedido
+        public List<PedidoItem> PedidosItens { get; set; }
+    }
+}
